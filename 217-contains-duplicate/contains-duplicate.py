9c7fa4ca38a514073b077ee532af40323a77c16d
Anurag_312006 <anuragghosh.312006@gmail.com>
2026-06-10 03:56:53 +0530

    Time: 19 ms (60.53%) | Memory: 26 MB (28.80%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hash=set()
+        for n in nums:
+            if n in hash:
+                return True
+            hash.add(n)
+        return False 
+        
